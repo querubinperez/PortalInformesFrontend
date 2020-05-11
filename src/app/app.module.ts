5453ea7d1b6d9e1c6c51d3bcc92fbd19254e0e6b
JuanQuerubin <Querubin.perez@udea.edu.co>
2020-05-11 17:06:13 -0500

    Se agrega componentes para listar los informes cargados y para mostrar el detalle de cada informe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,46 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 import {AppComponent} from './app.component';
 import {PlanoCargaComponent} from './plano-carga/plano-carga.component';
+import {InformeSismedComponent} from './informe-sismed/informe-sismed.component';
 import {MessagesComponent} from './messages/messages.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {DetalleInformeComponent} from './detalle-informe/detalle-informe.component';
+
+export const routes = [
+  {path: 'plano-carga', component: PlanoCargaComponent, label: 'Carga Archivo Plano'},
+  {path: 'informe-sismed', component: InformeSismedComponent, label: 'Informes Sismed'},
+  {path: 'detalle/:codigo', component: DetalleInformeComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PlanoCargaComponent,
-    MessagesComponent
+    MessagesComponent,
+    InformeSismedComponent,
+    TopBarComponent,
+    DetalleInformeComponent
   ],
   imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
